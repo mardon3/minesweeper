7ee049d2bbf3c9e170a91a63973a2d94357653cd
Mardon Usmanov <musmanov@unc.edu>
2023-09-25 18:48:59 -0400

    Background colors

---
+++ model/colors.go
@@ -0,0 +1,9 @@
+package model
+
+import "image/color"
+
+// Background colors of the GUI
+var (
+	HeaderBackgroundColor = color.RGBA{18, 47, 91, 255}
+	BoardBackgroundColor = 	color.RGBA{52, 172, 252, 255}
+)
