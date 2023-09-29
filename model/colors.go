2591de7272ee8ed7095f26a505cfd19e88e20c49
Mardon Usmanov <musmanov@unc.edu>
2023-09-29 15:34:51 -0400

    RBGA -> NRGBA

--- model/colors.go
+++ model/colors.go
@@ -4,6 +4,6 @@ import "image/color"
 
 // Background colors of the GUI
 var (
-	HeaderBackgroundColor = color.RGBA{18, 47, 91, 255}
-	BoardBackgroundColor = 	color.RGBA{52, 172, 252, 255}
+	HeaderBackgroundColor = color.NRGBA{18, 47, 91, 255}
+	BoardBackgroundColor = 	color.NRGBA{52, 172, 252, 255}
 )
