7098f95385e7d66c54b68944048c38ac5cf49776
Mardon Usmanov <musmanov@unc.edu>
2023-10-04 02:44:15 -0400

    Columns & rows of board layout depend on board (2d array) dimensions

--- view/boardview.go
+++ view/boardview.go
@@ -1,32 +1,51 @@
 package view
 
 import (
+	"musmanov/minesweeper/controller"
 	"musmanov/minesweeper/model"
 
 	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 )
 
-/*
-The Grid Layout is built to position children in a rows and columns.
-*/
-func NewBoardContainer() *widget.Container {
+var (
+	BoardContainer *widget.Container = newBoardContainer();
+)
+
+func newBoardContainer() *widget.Container {
+	screenWidth := controller.GetBoardWidth()
+	screenHeight := controller.GetBoardHeight()
+
+	columnStretchSlice := make([]bool, screenWidth)
+	for i := range columnStretchSlice {
+		columnStretchSlice[i] = true
+	}
+
+	rowStretchSlice := make([]bool, screenHeight)
+	for i := range rowStretchSlice {
+		rowStretchSlice[i] = true
+	}
+
 	boardContainer := widget.NewContainer(
-		// the container will use a plain color as its background
+		// the container will use assigned background color
 		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(model.BoardBackgroundColor)),
-		// the container will use an anchor layout to layout its single child widget
+		// container will generate as many columns as the screen width requires
 		widget.ContainerOpts.Layout(widget.NewGridLayout(
 			//Define number of columns in the grid
-			widget.GridLayoutOpts.Columns(1),
+			widget.GridLayoutOpts.Columns(screenWidth),
 			//Define how much padding to inset the child content
 			widget.GridLayoutOpts.Padding(widget.NewInsetsSimple(4)),
 			//Define how far apart the rows and columns should be
 			widget.GridLayoutOpts.Spacing(0, 0),
 			//Define how to stretch the rows and columns. Note it is required to
 			//specify the Stretch for each row and column.
-			widget.GridLayoutOpts.Stretch([]bool{true}, []bool{false, true}),
+			widget.GridLayoutOpts.Stretch(columnStretchSlice, rowStretchSlice),
 		)),
 	)
 
 	return boardContainer
+}
+
+func RenderBoard() *widget.Container {	
+	return BoardContainer
 }
