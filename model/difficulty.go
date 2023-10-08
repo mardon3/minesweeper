a8f71201cf0c94e731a28180a5226c6803c50600
Mardon Usmanov <musmanov@unc.edu>
2023-10-08 02:05:04 -0400

    Added CellSize field

--- model/difficulty.go
+++ model/difficulty.go
@@ -7,11 +7,12 @@ type Difficulty struct {
     Mines           int
     ScreenWidth     int
     ScreenHeight    int
+    CellSize        int
 }
 
 // Different difficulty levels
 var (
-    Beginner     = Difficulty{Height: 9, Width: 9, Mines: 10, ScreenWidth: 560, ScreenHeight: 632}
-    Intermediate = Difficulty{Height: 16, Width: 16, Mines: 40, ScreenWidth: 738, ScreenHeight: 810}
-    Expert       = Difficulty{Height: 16, Width: 30, Mines: 99, ScreenWidth: 738, ScreenHeight: 810}
+    Beginner     = Difficulty{Height: 9, Width: 9, Mines: 10, ScreenWidth: 560, ScreenHeight: 632, CellSize: 60}
+    Intermediate = Difficulty{Height: 16, Width: 16, Mines: 40, ScreenWidth: 738, ScreenHeight: 810, CellSize: 44}
+    Expert       = Difficulty{Height: 16, Width: 30, Mines: 99, ScreenWidth: 738, ScreenHeight: 810, CellSize: 44}
 )
